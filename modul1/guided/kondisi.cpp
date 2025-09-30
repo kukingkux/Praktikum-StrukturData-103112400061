1462d2d3575d6681ff111f4de2fc2ee6c1cbaced
kukingkux <cbmamangbae@gmail.com>
2025-09-30 18:17:11 +0700

    modul1

---
+++ modul1/guided/kondisi.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     double tot_pembelian, diskon;
+//     cout << "total pembelian: Rp";
+//     cin >> tot_pembelian;
+//     diskon = 0;
+//     if (tot_pembelian >= 100000)
+//         diskon = 0.05 * tot_pembelian;
+//     cout << "besar diskon = Rp" << diskon;
+// }
+
+
+
+// int main()
+// {
+//     double tot_pembelian, diskon;
+//     cout << "total pembelian: Rp";
+//     cin >> tot_pembelian;
+//     diskon = 0;
+//     if (tot_pembelian >= 100000)
+//         diskon = 0.05 * tot_pembelian;
+//     else
+//         diskon = 0;
+//     cout << "besar diskon = Rp" << diskon;
+// }
+
+
+
+int main()
+{
+    int kode_hari;
+    cout << "Menentukan hari kerja/libur\n"<<endl;
+    cout << "1=Senin 3=Rabu 5=Jumat 7=Minggu "<<endl;
+    cout << "2=Selasa 4=Kamis 6=Sabtu "<<endl;
+    cin >> kode_hari;
+    switch (kode_hari)
+    {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        cout<<"Hari Kerja";
+        break;
+    case 6:
+    case 7:
+        cout<<"Hari Libur";
+        break;
+    default:
+        cout<<"Kode masukan salah!!!";
+    }
+    return 0;
+}
---
+++ modul1/unguided/unguided1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float angka1, angka2;
+    float tambah, kurang, kali, bagi; // Keishin
+
+    cout << "Masukkan angka pertama: ";
+    cin >> angka1;
+    cout << "Masukkan angka kedua: ";
+    cin >> angka2;
+
+    tambah = angka1 + angka2;
+    kurang = angka1 - angka2;
+    kali = angka1 * angka2;
+    bagi = angka1 / angka2;
+
+    cout << "Hasil tambah: " << tambah << endl;
+    cout << "Hasil kurang: " << kurang << endl; // 103112400061
+    cout << "Hasil kali: " << kali << endl;
+    cout << "Hasil bagi: " << bagi << endl;
+
+    return 0;
+}
